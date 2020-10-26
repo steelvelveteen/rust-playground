9059568c5d905131c90cd875c26e5eb8fd0e608a
steelvelveteen <joeyvico@gmail.com>
2020-10-26 20:31:37 +1030

    If statements and the if to the right of a let statement

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,20 @@
 
+fn main() {
+    let number = 3;
+
+    // Nothing too glorious only that there are no braces
+    // around the condition in the if expression
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // Fun part here
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    
+     // Because if is an expression it can be used on the right
+     // side of a let statement
+    println!("The value of number is: {}", number);
+}
