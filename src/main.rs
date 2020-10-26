f5a423b2acba30160cbd0db18ac68c70db0b0faa
steelvelveteen <joeyvico@gmail.com>
2020-10-26 20:21:48 +1030

    Functions branch with a very basic example. Nothing more to add here

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-// mod print;
-// mod variables;
-mod types;
-
 fn main() {
-    // print::run();
-    // variables::run();
-    types::run();
+    // Function that takes parameters
+    let x: i32 = 16;
+    let y: i32 = 16;
+    let result = function_with_params(x, y);
+    println!("{}", result);
+}
+
+// In function signatures you MUST declare the type of each parameter
+fn function_with_params(x: i32, y: i32) -> i32 {
+    x * y
 }
