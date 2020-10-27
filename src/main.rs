7838b6942c10428e1f484cca7ab70e02b7739da0
steelvelveteen <joeyvico@gmail.com>
2020-10-27 11:24:10 +1030

    Understanding referenced returned values

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-// mod print;
-// mod variables;
-mod types;
+// Understanding stack vs heap in Rust
+// All data on the stack must a have a known, fixed size
+// The heap allocates a certain amount of space by request
+
+// Ownership rules
+// 1 Each value in Rust has variable that's called its owner
+// 2 There can only be one owner at a time
+// 3 When owner goes out of scope value is dropped
+
+// This branch is actually about the usage of references
 
 fn main() {
-    // print::run();
-    // variables::run();
-    types::run();
+    let mut name_string = String::from("Joey Vico");
+    println!("Original name: {}", name_string);
+
+    name_string = change_name(&mut name_string).to_string();
+    println!("Changed name: {}", name_string);
+}
+
+fn change_name(name: &mut String) -> &String{
+    name.push_str(" Lopez");
+    name
 }
