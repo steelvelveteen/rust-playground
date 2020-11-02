859e59bb10573d5a08945e177b40f8191f321c8f
steelvelveteen <joeyvico@gmail.com>
2020-11-03 08:45:14 +1030

    Basic structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+// For now here I am using String objects
+// slices require lifetime specifier
 fn main() {
-    println!("Hello, world!");
+    let user = User {
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+        active: true,
+    };
+
+    println!("{:#?}", user);
 }
