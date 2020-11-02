12c8c657e7e2d2912d2ebf5e3aa4d95a3220f326
steelvelveteen <joeyvico@gmail.com>
2020-11-03 08:46:05 +1030

    Resetting master branch

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-#[derive(Debug)]
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-}
-
-// For now here I am using String objects
-// slices require lifetime specifier
 fn main() {
-    let user = User {
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
-        active: true,
-    };
-
-    println!("{:#?}", user);
+    println!("Hello World!");
 }
