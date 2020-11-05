4563676adcc57d92e399ded58949234c1e13a9d1
steelvelveteen <joeyvico@gmail.com>
2020-11-05 17:14:16 +1030

    Extracting the value contained in an enum

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,13 @@ fn main() {
 
     println!("Ip Address type V4: {:#?}", addr);
     println!("Ip Address type V6: {:#?}", addr2);
+
+    // Found a way to print the string value contained in an 
+    // enum using Option<T>
+    let addr = Some(IpAddr::V4(String::from("Some string")));
+    match addr {
+        Some(IpAddr::V4(val)) | Some(IpAddr::V6(val)) => println!("{}", val),
+        // Some(IpAddr::V6(val)) => println!("{}", val),
+        None => println!("No"),
+    }
 }
