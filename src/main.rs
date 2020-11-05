cb1a538f0be659678f784cdd49a1e0b1aac84da0
steelvelveteen <joeyvico@gmail.com>
2020-11-05 17:03:38 +1030

    Basic enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
 fn main() {
-    println!("Hello World!");
+    let addr = IpAddr::V4("v4 ip address".to_string());
+    let addr2 = IpAddr::V6(String::from("V6 ip address"));
+
+    println!("Ip Address type V4: {:#?}", addr);
+    println!("Ip Address type V6: {:#?}", addr2);
 }
