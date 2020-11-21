b8d46800933e3c92ce5da17c950a3b1c6aabe4dc
steelvelveteen <joeyvico@gmail.com>
2020-11-22 09:54:20 +1030

    match if let pattern example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// Sorry, no match here. What I am exploring here is the use of the
+// if let construct to avoid having to write all the boilerplate code of a match
+// to exhaust all possiblities
+
+use std::io;
+
 fn main() {
-    
+    let some_u8_value = Some('N');
+    if let Some('y') | Some('Y') = some_u8_value {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+
+    println!("Enter first name: ");
+    let mut first_name = String::new();
+    io::stdin()
+        .read_line(&mut first_name)
+        .expect("Failed to enter name");
+    println!("{}", first_name);
 }
