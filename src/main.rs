6cda6e96c73a10b300999eb815138a38ce68f18b
steelvelveteen <joeyvico@gmail.com>
2021-03-21 17:25:00 +1030

    Using piston_window library Chris Hawkes tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-fn main() {
-    // Function that takes parameters
-    let x: i32 = 16;
-    let y: i32 = 16;
-    let result = function_with_params(x, y);
-    println!("{}", result);
-}
+extern crate piston_window;
 
-// In function signatures you MUST declare the type of each parameter
-fn function_with_params(x: i32, y: i32) -> i32 {
-    x * y
+use piston_window::*;
+
+fn main() {
+    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |context, graphics, _| {
+            clear([1.0; 4], graphics);
+            rectangle(
+                [1.0, 0.0, 0.0, 1.0],
+                [0.0, 0.0, 100.0, 100.0],
+                context.transform,
+                graphics,
+            );
+        });
+    }
 }
