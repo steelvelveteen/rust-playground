4bb9c5880488ba49104ab48d5c571a59bd377312
joeyOnMac <joeyvico@gmail.com>
2021-05-20 20:38:48 +0930

    Sandbox crap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
-    // Testing new environment
-    // Storing vs caching
+    let some_bool = true;
+    let isfiftyfive = if some_bool {
+        "It is 55"
+    } else {
+        "It's something else"
+    };
+
+    println!("{}", isfiftyfive);
 }
